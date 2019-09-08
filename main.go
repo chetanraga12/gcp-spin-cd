5f93c02cc96a429ab4c8d90a7abcfb1f397ba8fb
Kamal <skamalj@outlook.com>
2019-09-08 20:21:03 +0530

    test

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World!")
 }
+
+
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":80", nil)
